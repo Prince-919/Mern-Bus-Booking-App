6c0da270da095d75f52de2da34938a2be1410dd1
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2023-08-23 15:11:35 +0530

    mern-bus-booking-app

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const color = require("colors");
+const path = require("path");
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+const port = process.env.PORT || 8000;
+app.use(express.json());
+
+const usersRoute = require("./routes/usersRoute");
+const busesRoute = require("./routes/busesRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
+
+app.use("/api/users", usersRoute);
+app.use("/api/buses", busesRoute);
+app.use("/api/bookings", bookingsRoute);
+
+// Deployment Part Start
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
+// Deployment Part End
+
+app.listen(port, () =>
+  console.log(`Node server listening on port ${port}!`.bgCyan)
+);
